04e0405384c96e9dfbaa83439b9804798cf39836
TakahiroHimi <takahiro.mth.1209@gmail.com>
2021-03-23 22:23:31 +0900

    fix: client定義場所変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,16 @@ import { GraphQLClient, gql } from "graphql-request";
 import useSWR, { mutate } from "swr";
 import { signIn, signOut, useSession } from "next-auth/client";
 import { addReactionQuery } from "./queries";
+import { useState } from "react";
 
 const API = "https://api.github.com/graphql"; // GraphQLエンドポイントのURL
 
 const subjectId = "MDU6SXNzdWUyMzEzOTE1NTE="; // リアクションするIssueのID(https://github.com/octocat/Hello-World/issues/349)
 const content = "HOORAY"; // 付与するリアクションの種類
 
-const addReaction = (accessToken: string) => {
-  const client = new GraphQLClient(API, {
-    headers: {
-      Authorization: "bearer " + accessToken,
-    },
-  });
+const client = new GraphQLClient(API);
 
+const addReaction = (accessToken: string) => {
   void client.request(addReactionQuery, {
     addReactionInput: {
       subjectId: subjectId,
@@ -25,6 +22,13 @@ const addReaction = (accessToken: string) => {
 
 const IssuesPage = () => {
   const [session, loading] = useSession();
+  // const { data, error } = useSWR([JOB_POST_BY_ID_QUERY, id], (query, id) => request('/api', query, { id }));
+
+  // const { data, error, revalidate, isValidating, mutate } = useSWR();
+
+  if (session) {
+    client.setHeader("Authorization", "bearer " + session.accessToken);
+  }
 
   return (
     <>
@@ -43,8 +47,8 @@ const IssuesPage = () => {
         )}
         {session && (
           <>
-            Signed in as <img src={session.user.image ?? ""} width="50px" />
-            　{session.user.name} <br />
+            Signed in as <img src={session.user.image ?? ""} width="50px" />　
+            {session.user.name} <br />
             <button onClick={() => signOut()}>Sign out</button>
             <br />
             <button onClick={() => addReaction(session.accessToken ?? "")}>
